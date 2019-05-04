e6efcb02ff35c50121eaf9955c0b6f4025480149
Yong He <153747610@qq.com>
2019-05-04 13:14:38 +0800

    go format

--- echosvr/main.go
+++ echosvr/main.go
@@ -4,13 +4,10 @@ import (
 	"sync"
 )
 
-
 func main() {
-	s := NewServer("tcp", "127.0.0.1", 8081);
-	var wg sync.WaitGroup;
-	wg.Add(1);
-	s.Start();
-	wg.Wait();
+	s := NewServer("tcp", "127.0.0.1", 8081)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	s.Start()
+	wg.Wait()
 }
-
-
